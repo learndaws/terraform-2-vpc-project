9022443782c085d4f4777c24afafb8fbf0ebb61a
learndaws <learndaws@gmail.com>
2024-06-02 15:36:52 +0530

    all

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.52.0"
     }
   }
+
+  backend "s3" {
+    bucket = "daws-common-bucket"
+    key    = "common"
+    region = "us-east-1"
+    dynamodb_table = "daws-common-tfstate"
+  }
 }
 
 provider "aws" {
